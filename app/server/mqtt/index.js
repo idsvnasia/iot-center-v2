1e81e3cd796f25578c3d3a9d1b8ce6b01728c417
Pavel Zavora <pavel.zavora@bonitoo.io>
2021-05-25 07:40:47 +0200

    feat(app/server): implement MQTT proxy

---
+++ app/server/mqtt/index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const {MQTT_TOPIC} = require('../env')
+const createClient = require('./createClient')
+
+// returns a router instance using an MQTT client configured from env
+async function mqttRouter() {
+  const client = await createClient()
+  const router = express.Router()
+  // bigger bodies are expected
+  router.use(express.text({limit: '10mb'}))
+
+  // register an InfluxDB-compatible write endpoint forwards data to MQTT
+  router.post('/api/v2/write', async (req, res) => {
+    if (!client) {
+      res.status(500)
+      res.end('MQTT is not configured!')
+      return
+    }
+    const influxLineProtocolData = req.body
+    try {
+      await client.publish(MQTT_TOPIC, influxLineProtocolData)
+    } catch (e) {
+      res.status(500)
+      res.end('MQTT producer error: ' + e)
+      return
+    }
+    res.status(204)
+    res.end()
+  })
+}
+
+module.exports = mqttRouter
