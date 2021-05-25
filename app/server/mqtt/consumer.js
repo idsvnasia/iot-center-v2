775a41c4d709fdc0af3c5e9a2d31de18304c9755
Pavel Zavora <pavel.zavora@bonitoo.io>
2021-05-25 08:05:04 +0200

    feat(app/server): add MQTT consumer script

---
+++ app/server/mqtt/consumer.js
@@ -0,0 +1,27 @@
+const {MQTT_URL, MQTT_TOPIC} = require('../env')
+const createClient = require('./createClient')
+
+if (MQTT_URL && MQTT_TOPIC) {
+  ;(async function () {
+    const client = await createClient()
+    await client.subscribe(MQTT_TOPIC)
+    console.log('Subscribed to: ', MQTT_TOPIC)
+    client.on('message', function (_topic, buffer) {
+      const message = buffer.toString()
+      console.log(`${message.toString().split('\n').length} lines received`)
+    })
+    async function onShutdown() {
+      try {
+        await client.end()
+      } catch (error) {
+        console.error('ERROR: MQTT finalization failed', error)
+      }
+      // eslint-disable-next-line no-process-exit
+      process.exit(0)
+    }
+    process.on('SIGINT', onShutdown)
+    process.on('SIGTERM', onShutdown)
+  })().catch(console.error)
+} else {
+  console.log('Please specify both KAFKA_HOST and KAFKA_TOPIC')
+}
