9c38a17702c25e6a489846ebaa6a6f9886d47a81
Pavel Zavora <pavel.zavora@bonitoo.io>
2021-08-23 15:42:39 +0200

    chore(app/server): avoid ws proxy  issues in dev

--- app/ui/src/setupProxy.js
+++ app/ui/src/setupProxy.js
@@ -29,11 +29,11 @@ module.exports = function (app) {
       changeOrigin: true,
     })
   )
-  app.use(
-    '/mqtt',
-    createProxyMiddleware({
-      target: 'http://localhost:5000',
-      ws: true,
-    })
-  )
+  // app.use(
+  //   '/mqtt',
+  //   createProxyMiddleware({
+  //     target: 'http://localhost:5000',
+  //     ws: true,
+  //   })
+  // )
 }
