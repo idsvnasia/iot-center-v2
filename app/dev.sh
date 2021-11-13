fbe42d6b7ad7f90f7fc03975bb567a5da4af72a6
mirek.malecha@bonitoo.io <mirek.malecha@bonitoo.io>
2021-11-14 00:44:25 +0100

    docs: Add comments

--- app/dev.sh
+++ app/dev.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
 # Source code: https://github.com/bonitoo-io/iot-center-v2
-
 # INFLUX_URL
-# copy url from your web browser
-# example INFLUX_URL=https://us-west-2-1.aws.cloud2.influxdata.com
-
+#  copy url from your web browser
+#  example INFLUX_URL=https://us-west-2-1.aws.cloud2.influxdata.com
 # INFLUX_TOKEN
-# generated token in the InfluxDB UI
-# example INFLUX_TOKEN=h14b3X2n4kc8Q_jYPpwdjkv3dAZRorNQnN67pMwKs1lGgbMW8vWRjAi7VvkUitQMii2XwJM9qX3cnK4oAZDIjg==
-
+#  generated token in the InfluxDB UI
+#  example INFLUX_TOKEN=h14b3X2n4kc8Q_jYPpwdjkv3dAZRorNQnN67pMwKs1lGgbMW8vWRjAi7VvkUitQMii2XwJM9qX3cnK4oAZDIjg==
 # INFLUX_ORG
-# typically your email - can be changed via UI or API
-# example INFLUX_ORG=iotCenter@influxdata.com
+#  typically your email - can be changed via UI or API
+#  example INFLUX_ORG=iotCenter@influxdata.com
 
 export INFLUX_URL=
 export INFLUX_TOKEN=
@@ -34,4 +31,5 @@ if [ "$1" = "telegraf" ]; then
   exit
 fi
 
+export NODE_OPTIONS=--openssl-legacy-provider
 yarn dev
