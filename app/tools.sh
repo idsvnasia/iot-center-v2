e736c15977a20fcda7ce16ec3fc70cb6c26b67d1
mirek.malecha@bonitoo.io <mirek.malecha@bonitoo.io>
2021-11-18 18:57:45 +0100

    feat: add mosquitto installation

--- app/tools.sh
+++ app/tools.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+packageInstall () {
+  if [[ ! -z $YUM_CMD ]]; then
+    sudo yum install $1
+  elif [[ ! -z $APT_CMD ]]; then
+    sudo apt install $1
+  elif [[ ! -z $APT_GET_CMD ]]; then
+    sudo apt-get install $1
+  elif [[ ! -z $DNF_CMD ]]; then
+    sudo dnf install $1
+  elif [[ ! -z $PKG_CMD ]]; then
+    sudo pkg install $1
+  elif [[ ! -z $BREW_CMD ]]; then
+    brew install $1
+  else
+    echo "Error: cannot install $1 - unknow package manager!"
+  fi
+}
+
 #check available commands
 YUM_CMD=$(which yum)
 APT_CMD=$(which apt)
@@ -8,25 +26,18 @@ DNF_CMD=$(which dnf)
 PKG_CMD=$(which pkg)
 BREW_CMD=$(which brew)
 NPM_CMD=$(which npm)
+MQTT_CMD=$(which mosquitto)
 
 #install npm tool
 if [[ -z $NPM_CMD ]]; then
-	echo "Installing npm tool..."
-	if [[ ! -z $YUM_CMD ]]; then
-		sudo yum install npm
-	elif [[ ! -z $APT_CMD ]]; then
-		sudo apt install npm
-	elif [[ ! -z $APT_GET_CMD ]]; then
-		sudo apt-get install npm
-	elif [[ ! -z $DNF_CMD ]]; then
-		sudo dnf install npm
-	elif [[ ! -z $PKG_CMD ]]; then
-		sudo pkg install npm
-	elif [[ ! -z $BREW_CMD ]]; then
-		brew install npm
-	else
-		echo "Error: unknow package manager!"
-	fi
+  echo "Installing npm..."
+  packageInstall "npm"
+fi
+
+#install mosquitto tool
+if [[ -z $MQTT_CMD ]]; then
+  echo "Installing mosquitto..."
+  packageInstall "mosquitto"
 fi
 
 #install yarn and lateststable node.js
@@ -36,4 +47,4 @@ if [[ -z $BREW_CMD ]]; then
   echo "Installing node tool..."
   sudo npm install -g n
   sudo n stable
-fi	
+fi
